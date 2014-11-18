cacfa9a7f12457a645249b27f4a06d55df3b20c2
Théo <theolohues@gmail.com>
2014-11-18 10:41:31 +0100

    Commit pour la création de pdo_fonction.php

--- include/pdo_fonction.php
+++ include/pdo_fonction.php
@@ -1,3 +1,12 @@
 <?php
-
+	try {
+		$db = "mysql:host=localhost;dbname=clubamis";
+		$utilisateur = "root";
+		$mdp = "";
+		$connexion = new PDO($db, $utilisateur, $mdp); 
+		return $connexion;
+	}  // try
+	catch (PDOException $e) {
+		print("Erreur=".$e->getMessage());
+	} // fin catch
 ?>
