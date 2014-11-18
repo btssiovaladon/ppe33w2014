e25cd6f0e4d3f7f89464dcc2450b1c0588df8e97
Pierre Thuillier <pierre.thuillier2@gmail.com>
2014-11-18 11:08:33 +0100

    Fonction getParticipantDiner

--- include/pdo_fonction.php
+++ include/pdo_fonction.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+Permet d'ouvrir une connexion
+*/
+
 function fun_connexion_pdo() {
 	try {
 		$db = "mysql:host=localhost;dbname=clubamis";
@@ -11,6 +15,19 @@ function fun_connexion_pdo() {
 	catch (PDOException $e) {
 		print("Erreur=".$e->getMessage());
 	} // fin catch
+}
+
+/*
+Permet de récupérer les participants d'un diner
+*/
+
+function fun_get_participant_by_diner($co, $id){
+
+	$obtenir = $co->prepare("SELECT * FROM participer p INNER JOIN amis a ON p.N_AMIS = a.N_AMIS WHERE N_DINER = :id");
+	$obtenir->execute(array("id" => $id));
+	
+	return $obtenir->fetchAll();
+
 }
 
 ?> 
