a78f1a02e433ad7f0db18a70adfe2e7502f16aa0
Pierre Thuillier <pierre.thuillier2@gmail.com>
2014-11-18 11:57:23 +0100

    Consultation Diner Fonction Participants

--- include/pdo_fonction.php
+++ include/pdo_fonction.php
@@ -37,6 +37,19 @@ function fun_get_diner ($co, $num) {
 
 }
 
+/**
+
+	-> Permet de récupérer les participants à un id
+	-> Retoyrnne un tableau avec les enregistrements correspondant
+
+ */
+ 
+ function fun_get_participant_by_diner($co, $id){
+ 
+	$requete = $co->prepare("SELECT * FROM participer p INNER JOIN diner d On p.N_DINER = d.N_DINER WHERE p.N_DINER = :id");
+	$requete->execute(array("id" => $id));
+	return $requete->fetchAll();
+ }
 
 /**
 	-> Permet de récupérer l'ensemble des amis
