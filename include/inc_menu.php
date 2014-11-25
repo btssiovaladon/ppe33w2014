832559d1fc20fb585dab6ee9a8098daabd6a08b0
Sébastien <sebastien.lefebvrepro@gmail.com>
2014-11-25 09:13:07 +0100

    index.php2

--- include/inc_menu.php
+++ include/inc_menu.php
@@ -1,4 +1,10 @@
-<?php
-
-
-?>
+							<nav>
+								<ul class="menu">
+									<li><a class="active" href="index.html">About</a></li>
+									<li><a href="services.html">Services</a></li>
+									<li><a href="areas.html">Areas of Practice</a></li>
+									<li><a href="clients.html">Clients</a></li>
+									<li><a href="faq.html">FAQ</a></li>
+									<li class="last-item"><a href="contacts.html">Contacts</a></li>
+								</ul>
+							</nav>
