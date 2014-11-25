afce3b7a46c3ff60aa63d3657257bd7fa5e4fa0a
Marsial <Dracon@MacBook-Air-de-Dracon.local>
2014-11-25 11:22:57 +0100

    Modif/Supp Amis

---
+++ page/form_afficher_amis.php
@@ -0,0 +1,53 @@
+<?php
+if(isset($_GET['N_AMIS'])){
+fun_suppr_amis($co,$_GET['N_AMIS']);
+//requête : on supprime de la table Arbitre la ligne où les variables correspondent
+header("Location: index.php?page=form_afficher_amis.php");
+//une fois la suppression faite, on actualise la page
+}
+$resultat =fun_afficher_amis ($co);
+?>
+<!--requête : on sélectionne toutes les informations se trouvant dans la table clubamis et on les place dans la variable résultat -->
+<table border="1"> //affichage du tableau
+         <tr> <!-- Ligne 1 -->
+		<td>No Ami</td> <!-- L1C1-->
+		<td>No Fonction</td> <!-- L1C2-->
+	    <td>Nom</td> <!-- L1C3-->
+		<td>Prenom</td> <!-- L1C4-->
+		<td>Tel Fixe</td> <!-- L1C5-->
+	    <td>Tel mobile</td> <!-- L1C6-->
+		<td>Email</td> <!-- L1C7-->
+		<td>Rue</td> <!-- L1C8-->
+	    <td>Ville</td> <!-- L1C9-->
+		<td>Date d'entree</td> <!-- L1C10-->
+	    <td>Montant verse</td> <!-- L1C11-->
+		<td>Parrain1</td> <!-- L1C12-->
+	    <td>Parrain2</td> <!-- L1C13-->
+        </tr>
+<?php foreach ($resultat as $ligne)
+	{
+	?>
+<!-- tant qu'il y a une ligne, on affiche la variable "résultat" -->
+        <tr> <!-- Ligne 2 -->
+		<td> <?php echo $ligne["N_AMIS"];?> </td> <!-- L2C1  -->
+		<td> <?php echo $ligne["N_FONCTION"];?> </td> <!-- L2C2  -->
+		<td> <?php echo $ligne["NOM_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["PRENOM_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["TEL_FIX_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["TEL_PORTABLE_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["EMAIL_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["RUE_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["VILLE_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["DATE_ENTREE_AMIS"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["MT_VERSE"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["PARRAIN_1"];?> </td> <!-- L2C3  -->
+		<td> <?php echo $ligne["PARRAIN_2"];?> </td> <!-- L2C3  -->
+<!-- Affichage des champs de la table dans les différentes colonnes -->
+<td> <a href="index.php?page=form_modif_ami.php?N_AMIS=<?php echo $ligne["N_AMIS"];?>"> Modifier </a> </td> 
+<td> <a onclick="return(confirm('Etes vous sur de vouloir supprimer cette personne ?'))" href="index.php?page=form_afficher_amis.php?N_AMIS=<?php echo $ligne["N_AMIS"];?>"> Suprimer </a> </td> 
+	</tr>
+	<?php
+	}
+?>
+<!-- dès qu'il n'y a plus de ligne, on ferme la variable resultat -->
+</table>
---
+++ page/form_modif_ami.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_POST["valider"])){
+fun_modif_amis($co,$_POST['F_NOAMIS'],$_POST['F_FONC'],$_POST['F_NAMIS'],$_POST['F_PAMIS'],$_POST['F_TELF'],$_POST['F_TELP'],$_POST['F_EMAIL'],$_POST['F_RUE'],$_POST['F_VILLE'],$_POST['F_DATE'],$_POST['F_MT'],$_POST['F_PARRAIN1'],$_POST['F_PARRAIN2']);
+header("Location: index.php?page=form_afficher_amis.php");
+//réactualise la page
+}
+$resultat =fun_afficher_ami ($co, $_GET['N_AMIS']);
+foreach ($resultat as $ligne)
+	{
+	?>
+<form method="POST" action="">
+<fieldset>
+		<label>No Ami : </label><input type="text" readonly="readonly" name="F_NOAMIS" value="<?php echo $ligne["N_AMIS"];?>"/> <BR /> 
+		<label>No Fonction : </label><input type="text" name="F_FONC" value="<?php echo $ligne["N_FONCTION"];?>"/> <BR />
+		<label>Nom : </label><input type="text" name="F_NAMIS" value="<?php echo $ligne["NOM_AMIS"];?>"/> <BR />
+		<label>Prenom : </label><input type="text" name="F_PAMIS" value="<?php echo $ligne["PRENOM_AMIS"];?>"/> <BR />
+		<label>Tel Fixe : </label><input type="text" name="F_TELF" value="<?php echo $ligne["TEL_FIX_AMIS"];?>"/> <BR />
+		<label>Tel Mobile : </label><input type="text" name="F_TELP" value="<?php echo $ligne["TEL_PORTABLE_AMIS"];?>"/> <BR />
+		<label>Email : </label><input type="text" name="F_EMAIL" value="<?php echo $ligne["EMAIL_AMIS"];?>"/> <BR /> 
+		<label>Rue : </label><input type="text" name="F_RUE" value="<?php echo $ligne["RUE_AMIS"];?>"/> <BR />
+		<label>Ville : </label><input type="text" name="F_VILLE" value="<?php echo $ligne["VILLE_AMIS"];?>"/> <BR />
+		<label>Date d'entree : </label><input type="text" name="F_DATE" value="<?php echo $ligne["DATE_ENTREE_AMIS"];?>"/> <BR />
+		<label>Montant verse : </label><input type="text" name="F_MT" value="<?php echo $ligne["MT_VERSE"];?>"/> <BR />
+		<label>Parrain1 : </label><input type="text" name="F_PARRAIN1" value="<?php echo $ligne["PARRAIN_1"];?>"/> <BR />
+		<label>Parrain2 : </label><input type="text" name="F_PARRAIN2" value="<?php echo $ligne["PARRAIN_2"];?>"/> <BR />
+
+<input type="submit" name="valider" value="valider">
+</fieldset>
+</form>
+	<?php
+	}
+?>
+<!-- dès qu'il n'y a plus de ligne, on ferme la variable resultat -->
+</table>
