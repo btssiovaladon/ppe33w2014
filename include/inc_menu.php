2397bfe7e5a892ffb9540535771360cc86a5a1e4
Pierre Thuillier <pierre.thuillier2@gmail.com>
2014-12-02 08:58:15 +0100

    Ajout include menu et entete

---
+++ include/inc_menu.php
@@ -0,0 +1,11 @@
+
+							<nav>
+								<ul class="menu">
+									<li><a class="active" href="">Gestion Amis</a></li>
+									<li><a href="">Actions</a></li>
+									<li><a href="?page=form_tableau_diner.php">Diners</a></li>
+									<li><a href="">Bureau</a></li>
+									<li><a href="">Financiere</a></li>
+									<li class="last-item"><a href="">Contacts</a></li>
+								</ul>
+							</nav>
