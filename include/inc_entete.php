8e9043dbaf9d43d09e78d6dbda558bc01ddc81cb
Pierre Thuillier <pierre.thuillier2@gmail.com>
2014-12-09 08:56:48 +0100

    Correction ajax

--- include/inc_entete.php
+++ include/inc_entete.php
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php
-	 include ("page/form_connexion.php"); 
-?>
-=======
-=======
-<?php
-	 include ("page/form_connexion.php"); 
-?>
->>>>>>> 5712d2f36547117f114727195361bc54fd982614
 
-<<<<<<< HEAD
-=======
 <?php
 	 include ("page/form_connexion.php"); 
 ?>
 
-<<<<<<< HEAD
->>>>>>> bda888a2537dcc3deb174b0c1b836b942604f09a
-=======
->>>>>>> 5712d2f36547117f114727195361bc54fd982614
->>>>>>> fa35543db01f57ddbb00a491e628e697a4541432
+
 				<div class="main">
 					<div class="border-top">
 						<div class="wrapper">
