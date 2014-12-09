f76ee9be0e89710073a74830340e1d566f053130
Sébastien <sebastien.lefebvrepro@gmail.com>
2014-12-09 09:25:46 +0100

    Mise a jour pag_actmembre

--- page/pag_actmembre_consult.php
+++ page/pag_actmembre_consult.php
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
-<table  border = '2' bordercolor = 'darkblue'>
-<tr>
-	<th>Code</th>
-	<th>Nom</th>
-	<th>Prenom</th>
-	<th>Role</th>
-	<th>Suprimer de l'activit&eacute;</th>
-</tr>
-<?php $resultat1=$co->query('SELECT amis.*, action.N_AMIS as n_chef FROM amis inner join action on amis.N_AMIS = action.N_AMIS ORDER BY amis.N_AMIS');
-	$resultat2=$co->query('SELECT amis.*, participant.N_AMIS as n_participant FROM amis inner join participant on amis.N_AMIS = participant.N_AMIS ORDER BY amis.N_AMIS');
-while($ligne=$resultat1->fetch()){
-?>
-<tr>
-	<td><?php echo $ligne['N_AMIS'];?></td>
-	<td><?php echo $ligne['NOM_AMIS'];?></td>
-	<td><?php echo $ligne['PRENOM_AMIS'];?></td>
-	<td><?php if($ligne['N_AMIS']==$ligne['n_chef']){
-=======
+<h3>
+	Liste des participants pour l'action <?php echo $_GET['id']?>
+</h3>
 <table  border = '2'>
 <tr>
 	<th>Code</th>
@@ -27,15 +11,33 @@ while($ligne=$resultat1->fetch()){
 </tr>
 <?php 
 $ligne1 = fun_obtenir_chef_action($co, $_GET['id']);
+
 ?>
 <tr>
 	<td><?php echo $ligne1['N_AMIS'];?></td>
 	<td><?php echo $ligne1['NOM_AMIS'];?></td>
 	<td><?php echo $ligne1['PRENOM_AMIS'];?></td>
-	<td><?php if($ligne1['N_AMIS']==$ligne1['n_chef']){
->>>>>>> 5712d2f36547117f114727195361bc54fd982614
-		echo 'chef';}
-		else{echo "participant";}?></td>
+	<td>
+		<select name='role' id='role'>
+			<option selected='selected'>chef</option>
+			<option>participant</option>
+		</select>
+	</td>
+	<td><a href=''>X</a></td>
+</tr>
+<?php $resultat = fun_obtenir_participants_action($co, $_GET['id']);
+foreach($resultat as $ligne2){
+?>
+<tr>
+	<td><?php echo $ligne2['N_AMIS'];?></td>
+	<td><?php echo $ligne2['NOM_AMIS'];?></td>
+	<td><?php echo $ligne2['PRENOM_AMIS'];?></td>
+	<td>
+		<select name='role' id='role'>
+			<option>chef</option>
+			<option selected='selected'>participant</option>
+		</select>
+	</td>
 	<td><a href=''>X</a></td>
 </tr>
 <?php } ?>
