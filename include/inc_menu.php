7028fd31835e5844d10179fba5bb296f214299ba
unknown <simon.richard@B511_11.BTSSIO.LOCAL>
2014-12-16 09:47:35 +0100

    menu

--- include/inc_menu.php
+++ include/inc_menu.php
@@ -12,20 +12,20 @@
 				<li><a href="index.php?page=form_saisie_action.php">Saisie d’une action</a></li>
 				<li><a href="index.php?page=form_modification_action.php">Modification d’une action</a></li>
 				<li><a href="index.php?page=form_afficher_amis.php">Suppression d’une action</a></li>
-				<li><a href="#">Inscription d’un AMI à une action</a></li>
+				<li><a href="index.php?page=form_inscription_diner.php">Inscription d’un AMI à une action</a></li>
 				<li><a href="#">Edition de la liste des participants par action</a></li>
-				<li><a href="#">Consultation des membres de l’action</a></li>
+				<li><a href="index.php?page=pag_consult_membre_action.php">Consultation des membres de l’action</a></li>
 				<li><a href="#">Edition d’étiquettes</a></li>
 			</ul>
 		</li>
 		<li><a href="#">Gestion Diners</a>
 			<ul>
-				<li><a href="#">Création d’un dîner</a></li>
-				<li><a href="#">Modification/Suppression d’un dîner</a></li>
-				<li><a href="#">Inscription à un dîner</a></li>
-				<li><a href="#">Edition de la liste des participants à un dîner</a></li>
-				<li><a href="#">Edition de la liste des participants par action</a></li>
-				<li><a href="#">Consultation des participants à un dîner</a></li>
+				<li><a href="index.php?page=form_ajout_diner.php">Création d’un dîner</a></li>
+				<li><a href="index.php?page=form_edition_diner.php">Modification/Suppression d’un dîner</a></li>
+				<li><a href="index.php?page=form_inscription_diner.php">Inscription à un dîner</a></li>
+				<li><a href="index.php?page=form_edition_participatn_diner.php">Edition de la liste des participants à un dîner</a></li>
+				<li><a href="index.php?page=form_modification_action.php">Edition de la liste des participants par action</a></li>
+				<li><a href="index.php?page=form_consultation_diner.php">Consultation des participants à un dîner</a></li>
 			</ul>
 		</li>
 		<li><a href="#">Gestion du bureau / commissions</a>
@@ -33,17 +33,17 @@
 				<li><a href="#">Saisie du bureau</a></li>
 				<li><a href="#">Modification du bureau</a></li>
 				<li><a href="#">Création d’une commission</a></li>
-				<li><a href="#">Saisie du bureau de la commission</a></li>
-				<li><a href="#">Inscription d’un AMIS à une commission</a></li>
+				<li><a href="index.php?page=form_saisie_bureau_commission.php">Saisie du bureau de la commission</a></li>
+				<li><a href="index.php?page=form_inscription_amis_commissions.php">Inscription d’un AMIS à une commission</a></li>
 				<li><a href="#">Consultation des membres de la commission</a></li>
 				<li><a href="#">Edition pour une commission de la liste des actions</a></li>
 			</ul>
 		</li>
 		<li><a href="#">Gestion financière</a>
 			<ul>
-				<li><a href="#">Saisie de la cotisation annuelle</a></li>
-				<li><a href="#">Edition du relevé annuel</a></li>
-				<li><a href="#">Saisie du règlement d’un AMIS</a></li>
+				<li><a href="index.php?page=form_saisie_cotisation.php">Saisie de la cotisation annuelle</a></li>
+				<li><a href="index.php?pdf=pag_releve_annuel_pdf.php">Edition du relevé annuel</a></li>
+				<li><a href="index.php?page=form_saisie_montant.php">Saisie du règlement d’un AMIS</a></li>
 			</ul>
 		</li>
 </nav>
