2ccb865c76e743b5495a3acf0a0d17f11bba712b
unknown <simon.richard@B511_11.BTSSIO.LOCAL>
2014-12-16 11:40:14 +0100

    entete

--- include/inc_entete.php
+++ include/inc_entete.php
@@ -1,29 +1,4 @@
 
-<header>
-	<div class="main">
-		<div class="border-top">
-			<?php
-				include ("page/form_connexion.php"); 
-			?>
-		</div>
-	</div>
-
-	<div class="main">
-		<div class="border-top">
-			<div class="wrapper">
-				<?php
-					include("include/inc_menu.php");
-				?>
-			</div>
-		</div>
-		<div class="slider-bg">
-			<div class="slider">
-									                  
-			</div>
-		</div>
-	</div>
-</header>
-
 
 <?php
 	include ("page/form_connexion.php"); 
